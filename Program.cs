7eabd3cf3618ebbd8f3c9abb24c2303619903ae0
Lisa Perlova <libertas.lis@yandex.ru>
2022-12-13 22:06:34 +0500

    добавлено решение задачи

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿string[] array = new string[6] { "l", "hello", "world", "hsd", "123", ":)" };
+string[] newArray = new string[array.Length];
+
+void WordsWith3Symbols(string[] array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < 4)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+WordsWith3Symbols(array, newArray);
+PrintArray(newArray);
